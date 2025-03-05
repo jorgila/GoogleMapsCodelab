e679b63ce1eabb796f64398800e5fe8b75569d94
Jorge Gimeno <jgimenolatas@gmail.com>
2025-03-05 20:34:00 +0100

    Steps 7 to 8

--- app/src/main/java/com/example/mountainmarkers/MountainMap.kt
+++ app/src/main/java/com/example/mountainmarkers/MountainMap.kt
@@ -12,15 +12,25 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.example.mountainmarkers.presentation.MountainsScreenEvent
 import com.example.mountainmarkers.presentation.MountainsScreenViewState
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMapOptions
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 /**
  * Shows a [GoogleMap] with collection of markers
@@ -35,10 +45,29 @@ fun MountainMap(
     var isMapLoaded by remember { mutableStateOf(false) }
 
     // TODO: Create cameraPositionState
-
+    val cameraPositionState = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(viewState.boundingBox.center, 5f)
+    }
     // TODO: Create scope from rememberCoroutineScope
+    val scope = rememberCoroutineScope()
+
     //   Add LaunchedEffect to zoom when the bounding box changes
+
+    LaunchedEffect(key1 = viewState.boundingBox) {
+        zoomAll(scope, cameraPositionState, viewState.boundingBox)
+    }
+
     //   Add LaunchedEffect to react to events from the ViewModel
+
+    LaunchedEffect(true) {
+        eventFlow.collect { event ->
+            when(event){
+                MountainsScreenEvent.OnZoomAll -> {
+                    zoomAll(scope, cameraPositionState, viewState.boundingBox)
+                }
+            }
+        }
+    }
 
     Box(
         modifier = Modifier
@@ -49,7 +78,11 @@ fun MountainMap(
 
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            onMapLoaded = { isMapLoaded = true }
+            onMapLoaded = { isMapLoaded = true },
+            googleMapOptionsFactory = {
+                GoogleMapOptions().mapId("5498c55c1fa4107")
+            },
+            cameraPositionState = cameraPositionState
         )
         // TODO: Add cameraPositionState to GoogleMap
 
@@ -81,5 +114,18 @@ fun MountainMap(
 }
 
 // TODO: Create zoomAll function
+
+fun zoomAll(
+    scope: CoroutineScope,
+    cameraPositionState: CameraPositionState,
+    boundingBox: LatLngBounds
+) {
+    scope.launch {
+        cameraPositionState.animate(
+            update = CameraUpdateFactory.newLatLngBounds(boundingBox, 64),
+            durationMs = 1000
+        )
+    }
+}
 
 // TODO: Create ColoradoPolygon function
