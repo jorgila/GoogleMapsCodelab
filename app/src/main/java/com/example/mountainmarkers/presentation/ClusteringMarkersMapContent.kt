6eec99a58871f5d67176cf97e7cc6df92c1d15e8
Jorge Gimeno <jgimenolatas@gmail.com>
2025-03-18 12:08:45 +0100

    Steps 11

--- app/src/main/java/com/example/mountainmarkers/presentation/ClusteringMarkersMapContent.kt
+++ app/src/main/java/com/example/mountainmarkers/presentation/ClusteringMarkersMapContent.kt
@@ -14,21 +14,58 @@
 
 package com.example.mountainmarkers.presentation
 
+import android.provider.CalendarContract.Colors
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.mountainmarkers.R
 import com.example.mountainmarkers.data.local.Mountain
+import com.example.mountainmarkers.data.local.is14er
+import com.example.mountainmarkers.data.utils.LocalUnitsConverter
 import com.example.mountainmarkers.presentation.MountainsScreenViewState.MountainList
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterItem
+import com.google.maps.android.compose.GoogleMapComposable
+import com.google.maps.android.compose.MapsComposeExperimentalApi
+import com.google.maps.android.compose.clustering.Clustering
 
 // TODO: Create data class MountainClusterItem which implements a [ClusterItem]
 
+data class MountainClusterItem(
+    val mountain: Mountain,
+    val snippetString: String
+) : ClusterItem {
+    override fun getPosition() = mountain.location
+    override fun getTitle() = mountain.name
+    override fun getSnippet() = snippetString
+    override fun getZIndex() = 0f
+}
+
 // TODO: Create data class IconColor
+
+data class IconColor(val iconColor: Color, val backgroundColor: Color, val borderColor: Color)
 
 /**
  * [GoogleMapComposable] which renders a [MountainList] using the [Clustering] composable
  */
 // TODO: Add @GoogleMapComposable annotation
+@OptIn(MapsComposeExperimentalApi::class)
 @Composable
+@GoogleMapComposable
 fun ClusteringMarkersMapContent(
     mountains: List<Mountain>,
     onClusterClick: (Cluster<out ClusterItem>) -> Boolean = { false },
@@ -36,7 +73,64 @@ fun ClusteringMarkersMapContent(
 ) {
     // TODO: Create color schemes for fourteeners and non-fourteeners
 
+    val backgroundAlpha = 0.6f
+    val fourteenerColors = IconColor(
+        iconColor = MaterialTheme.colorScheme.onPrimary,
+        backgroundColor = MaterialTheme.colorScheme.primary.copy(alpha = backgroundAlpha),
+        borderColor = MaterialTheme.colorScheme.primary
+    )
+    val otherColors = IconColor(
+        iconColor = MaterialTheme.colorScheme.secondary,
+        backgroundColor = MaterialTheme.colorScheme.secondaryContainer.copy(alpha = backgroundAlpha),
+        borderColor = MaterialTheme.colorScheme.secondary
+    )
     // TODO: Create mountainClusterItems from each of the mountains
+
+    val unitsConverter = LocalUnitsConverter.current
+    val resources = LocalContext.current.resources
+
+    val mountainClusterItems by remember(mountains) {
+        mutableStateOf(
+            mountains.map { mountain ->
+                MountainClusterItem(
+                    mountain = mountain,
+                    snippetString = unitsConverter.toElevationString(resources, mountain.elevation)
+                )
+            }
+        )
+    }
+
+    Clustering(
+        items = mountainClusterItems,
+        clusterItemContent = { mountainItem ->
+            val colors = if (mountainItem.mountain.is14er()){
+                fourteenerColors
+            } else {
+                otherColors
+            }
+            SingleMountain(colors)
+        }
+    )
+
 }
 
 // TODO: Create SingleMountain composable to render a customized icon for an individual mountain
+
+@Composable
+private fun SingleMountain(
+    colors: IconColor,
+){
+    Icon(
+        painterResource(id= R.drawable.baseline_filter_hdr_24),
+        tint = colors.iconColor,
+        contentDescription = "",
+        modifier = Modifier
+            .size(32.dp)
+            .padding(1.dp)
+            .drawBehind {
+                drawCircle(color = colors.backgroundColor, style = Fill)
+                drawCircle(color = colors.borderColor, style = Stroke(width = 3f))
+            }
+            .padding(4.dp)
+    )
+}
